c16eb0cd3f2d58465136f5104f8e4c2b86f81184
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-11-04 15:49:59 +0530

    added some input handling, can only enter numbers in range [0,9]

--- program6/prog06.asm
+++ program6/prog06.asm
@@ -1,6 +1,7 @@
 section .data
 	msg1 db 0xa, 0xd, "Enter a Digit (0-9): "
 	msg2 db 0xa, 0xd, "Squared value is"
+	errormsg db 0xa, 0xd, "Error! Invalid Input"
 section .bss
 	inbuf resb 1
 	outbuf resb 2
@@ -8,6 +9,8 @@ section .text
 global _start
 
 _start:
+
+
 
 ;Input
 ;display msg1
@@ -18,12 +21,20 @@ mov ebx, 1	;stdout
 mov eax, 4	;syscall numeber sys_write
 int 0x80
 
+
 ;read from stdin (keyboard)
 mov ebx,0 	;stdin
 mov ecx, inbuf	;pointer to variable (storage-location)
 mov edx, 1	;number of bytes to rad
 mov eax, 3	;syscall number for sys_read
 int 0x80
+
+mov al, byte[inbuf]	;put the inbuff value in al so that the value in the inbuf could be used in cmp (cannot use inbuf directly in cmp)
+cmp al, '0' ; sets flags register
+jl ERRORMSG	; jump, if less than ascii value of 0, to ENDPROG (if al<0 jmp to ENDPROG)
+
+cmp al, '9'	;jump, if greater than ascii value of 9, to ENDPROG
+jg ERRORMSG ; jumps to the instructino at ENDPRG label
 
 mov al, byte[inbuf]
 sub al, '0'
@@ -50,9 +61,17 @@ mov eax, 4      ;syscall numeber sys_write
 int 0x80
 
 
-
+ENDPROG:
 MOV eax, 1
 MOV ebx, 30
 INT 80h;this is the interrupt address
 
 
+ERRORMSG:
+;write the results to  (console)
+mov edx,23      ;msg length
+mov ecx,errormsg   ;pointer to variable
+mov ebx, 1      ;stdout
+mov eax, 4      ;syscall numeber sys_write
+int 0x80
+jmp ENDPROG
