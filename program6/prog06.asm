b9ab442b72fc5e3ed8dfd9e0855ca8f7e0223545
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-11-04 14:20:52 +0530

    prog06.asm now squares the input and displays it

--- program6/prog06.asm
+++ program6/prog06.asm
@@ -1,7 +1,9 @@
 section .data
 	msg1 db 0xa, 0xd, "Enter a Digit (0-9): "
+	msg2 db 0xa, 0xd, "Squared value is"
 section .bss
 	inbuf resb 1
+	outbuf resb 2
 section .text
 global _start
 
@@ -9,18 +11,45 @@ _start:
 
 ;Input
 ;display msg1
+;write to stdout (console)
 mov edx,23	;msg length
 mov ecx, msg1	;pointer to variable
 mov ebx, 1	;stdout
 mov eax, 4	;syscall numeber sys_write
 int 0x80
 
-
+;read from stdin (keyboard)
 mov ebx,0 	;stdin
 mov ecx, inbuf	;pointer to variable (storage-location)
 mov edx, 1	;number of bytes to rad
 mov eax, 3	;syscall number for sys_read
 int 0x80
+
+mov al, byte[inbuf]
+sub al, '0'
+mul al
+mov bl, 10
+div bl
+add al, '0'
+add ah, '0'
+mov word[outbuf], ax 	; note that ax is the union of ah and al, we use word because we are sending more than one byte at once
+
+;display the outbuffer
+;write "Squared Value is" to stdout (console)
+mov edx,19      ;msg length
+mov ecx, msg2   ;pointer to variable
+mov ebx, 1      ;stdout
+mov eax, 4      ;syscall numeber sys_write
+int 0x80
+
+;write the results to  (console)
+mov edx,2      ;msg length
+mov ecx, outbuf   ;pointer to variable
+mov ebx, 1      ;stdout
+mov eax, 4      ;syscall numeber sys_write
+int 0x80
+
+
 
 MOV eax, 1
 MOV ebx, 30
